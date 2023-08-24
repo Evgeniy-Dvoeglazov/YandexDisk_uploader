c8c6be2f6ffa8016a7353464e802982c8371eeb2
Evgeniy Dvoeglazov <jindv@yandex.ru>
2023-08-24 11:43:08 +0300

    дебаг

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import { loadingReducer } from './loadingReducer';
 import { uploadSuccessReducer } from './uploadSuccessReducer';
 import { serverErrorReducer } from './serverErrorReducer';
 import { authReducer } from './authReducer';
+import { tokenReducer } from './tokenReducer';
 import { authErrorReducer } from './authErrorReducer';
 
 const rootReducer = combineReducers({
@@ -14,7 +15,8 @@ const rootReducer = combineReducers({
   uploadSuccess: uploadSuccessReducer,
   serverError: serverErrorReducer,
   auth: authReducer,
-  authError: authErrorReducer
+  authError: authErrorReducer,
+  token: tokenReducer
 })
 
 export const store = createStore(rootReducer);
--- src/store/tokenReducer.js
+++ src/store/tokenReducer.js
@@ -4,7 +4,7 @@ const initialState = {
   tokenInfo: {},
 }
 
-export const uploadSuccessReducer = (state = initialState, action) => {
+export const tokenReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOKEN:
       return {
